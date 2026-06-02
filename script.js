e6b7b3bc3912c10c7276c975772b1cd73a375a5d
ssssssnlkf <farohramadhani12@gmail.com>
2026-06-02 13:04:21 +0700

    Add function to show active content section

---
+++ script.js
@@ -0,0 +1,15 @@
+function showSection(id){
+
+  const sections =
+  document.querySelectorAll(".content-section");
+
+  sections.forEach(section => {
+    section.classList.remove("active");
+  });
+
+  document
+    .getElementById(id)
+    .classList.add("active");
+}
+
+showSection("struktur");
